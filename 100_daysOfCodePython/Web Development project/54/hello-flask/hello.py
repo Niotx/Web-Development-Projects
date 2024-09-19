bffe145f87448ed87c02167fd03c85e246f4752a
Niotx <mmnaseri4444@gmail.com>
2024-09-19 11:54:19 +0330

    56.0

--- 100_daysOfCodePython/Web Development project/54/hello-flask/hello.py
+++ 100_daysOfCodePython/Web Development project/54/hello-flask/hello.py
@@ -1,5 +1,21 @@
 from flask import Flask
 
+
+def make_bold(func):
+    def wraper(*args, **kwargs):
+        return "<b>" + func() + "</b>"
+    return wraper
+
+def make_emphasis(func):
+    def wraper():
+        return "<em>" + func() + "<em>"
+    return wraper
+
+def make_underlined(func):
+    def wraper():
+        return "<u>" + func() + "</u>"
+    return wraper
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,9 +23,18 @@ def hello_world():
     return '<h1 style="text-align:center">Hello, World!'\
         ' </h1> <p>This is a paragraph.</p>'
 
-@app.route('/<name>/<int:age>')
-def bye(name,age):
-    return f"you are {name} and you are {age} years old"
+@app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underlined
+def decorator_t():
+    return "bye"
+
+
+
+# @app.route('/<name>/<int:age>')
+# def bye(name,age):
+#     return f"you are {name} and you are {age} years old"
 
 if __name__ == "__main__":
     app.run(debug=True)
