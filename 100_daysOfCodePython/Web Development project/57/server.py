06a50f8c13ebf5e4164570262a1a9749603f9f6a
Niotx <mmnaseri4444@gmail.com>
2024-09-20 23:17:47 +0330

    57.2

--- 100_daysOfCodePython/Web Development project/57/server.py
+++ 100_daysOfCodePython/Web Development project/57/server.py
@@ -1,14 +1,26 @@
 from flask import Flask,render_template
 import random
 import datetime
+import requests
+import re
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    random_number = random.randint(1, 10)
-    year = datetime.date.today().year
-    return render_template('index.html', num=random_number, year=year)
 
+# https://api.agify.io?name=Tom
+# https://api.genderize.io?name=Tom
+# @app.route('/')
+# def hello():
+#     random_number = random.randint(1, 10)
+#     year = datetime.date.today().year
+#     return render_template('index.html', num=random_number, year=year)
+
+@app.route('/guess/<name>')
+def guess(name):
+    agify = {}
+    agify = requests.get(f'https://api.agify.io?name={name}')
+    genderize = {}
+    genderize = requests.get(f'https://api.genderize.io?name={name}')
+    return render_template('index.html', age=agify, gender=genderize)
 if __name__ == '__main__':
     app.run(debug=True)
