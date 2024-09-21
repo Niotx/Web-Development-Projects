9ffd0efe3328ae746fa468837b7a0ac147824be1
Niotx <mmnaseri4444@gmail.com>
2024-09-21 13:01:39 +0330

    57.f

--- 100_daysOfCodePython/Web Development project/57/006 day-57-starting-files-blog-templating/main.py
+++ 100_daysOfCodePython/Web Development project/57/006 day-57-starting-files-blog-templating/main.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template,url_for
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    URL = 'https://api.npoint.io/c790b4d5cab58020d391'
+    response = requests.get(URL)
+    responseData = response.json()
+    return render_template("index.html", posts=responseData)
+
+@app.route('/blog/<int:blog_id>')
+def get_blog(blog_id):
+    URL = 'https://api.npoint.io/c790b4d5cab58020d391'
+    response = requests.get(URL)
+    responseData = response.json()
+    return render_template('post.html', posts=responseData, id=blog_id - 1)
 
 if __name__ == "__main__":
     app.run(debug=True)
