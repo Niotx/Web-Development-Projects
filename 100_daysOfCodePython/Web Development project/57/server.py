cdba792d0bb130a075093fad77b24d0d9bc5db8f
Niotx <mmnaseri4444@gmail.com>
2024-09-21 12:06:45 +0330

    57.3

--- 100_daysOfCodePython/Web Development project/57/server.py
+++ 100_daysOfCodePython/Web Development project/57/server.py
@@ -1,8 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 import random
 import datetime
 import requests
-import re
 
 app = Flask(__name__)
 
@@ -21,6 +20,16 @@ def guess(name):
     agify = requests.get(f'https://api.agify.io?name={name}')
     genderize = {}
     genderize = requests.get(f'https://api.genderize.io?name={name}')
-    return render_template('guess.html', age=agify, gender=genderize)
+    return render_template('guess.html',persen_name=name, age=agify, gender=genderize)
+
+
+@app.route('/blog')
+def blog():
+    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template('blog.html', post=all_posts)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
